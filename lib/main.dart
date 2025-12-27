0e440862ef7f21bfebc16d43e37e7b8dd30ba290
Gem <mojeedibrahim13@gmail.com>
2025-12-26 18:35:03 -0800

    MaterialApp added

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,10 @@ class MyApp extends StatelessWidget{
   const MyApp({super.key});
   @override
   Widget build(BuildContext context){
-    return const Text(
-      "Hello World!!!", textDirection: TextDirection.ltr,
+    return const MaterialApp(
+      home: Scaffold(
+        body: Text("Hello World!!!"),
+      ),
     );
   }
 }
