0e8a14ccd31ce1980ff81d75dd902e2c96c38af6
Gem <mojeedibrahim13@gmail.com>
2025-12-30 02:32:01 -0800

    done with all UI

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -47,7 +47,7 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
                   decoration: InputDecoration(
                     hintText: "Please Enter athe Amount in USD",
                     hintStyle: TextStyle(
-                      color: const Color.fromARGB(255, 255, 255, 255),
+                      color: const Color.fromARGB(255, 0, 98, 255),
                     ),
                     prefixIcon: Icon(Icons.monetization_on),
                     prefixIconColor: const Color.fromARGB(255, 0, 98, 255),
@@ -70,7 +70,7 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
                 },
                 style: TextButton.styleFrom(
                   backgroundColor: const Color.fromARGB(255, 0, 98, 255),
-                  foregroundColor: const Color.fromARGB(255, 0, 98, 255),
+                  foregroundColor: const Color.fromARGB(255, 255, 255, 255),
                   minimumSize: Size(double.infinity, 50),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(5),
